79d52d4453caadf410696749de30a4a7794bc518
danewheaton <wheaton.dane@gmail.com>
2017-02-04 23:43:12 -0600

    adding mirror project

---
+++ Mirrors/Assets/Scripts/PlayerTest1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTest1 : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Mirror1")
+        {
+            print("should hit mirror");
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, -transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "FallTrigger")
+        {
+            transform.position = spawnPoint.position;
+        }
+    }
+}
