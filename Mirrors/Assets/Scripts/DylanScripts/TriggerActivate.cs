5e6e5d89030b49f415634176f1391bff4d39defb
dboggsIAM <dylan.boggs@loop.colum.edu>
2017-03-11 18:27:30 -0600

    Dylan - Script Updates
    Pushing the script changes to the scene, everything should be right as
    rain now

---
+++ Mirrors/Assets/Scripts/DylanScripts/TriggerActivate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerActivate : MonoBehaviour
+{
+
+
+    public GameObject[] m_ActivatedObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        MeshRenderer meshRender = this.gameObject.GetComponent<MeshRenderer>();
+        //Make self invisible
+        meshRender.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < m_ActivatedObjects.Length; i++)
+        {
+            m_ActivatedObjects[i].SetActive(true);
+        }
+    }
+}
---
+++ Mirrors/Assets/Scripts/DylanScripts/TriggerDeactivate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDeactivate : MonoBehaviour {
+
+
+    public GameObject[] m_DeactivatedObjects;
+
+	// Use this for initialization
+	void Start ()
+    {
+        MeshRenderer meshRender = this.gameObject.GetComponent<MeshRenderer>();
+        //Make self invisible
+        meshRender.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < m_DeactivatedObjects.Length; i++)
+        {
+            m_DeactivatedObjects[i].SetActive(false);
+        }
+    }
+}
